e0c792b5a501ec9ff5ed4ddf766c202632d64ec5
georgestrenghtuk-cell <georgestrenghtuk@gmail.com>
2026-05-22 16:01:47 +0200

    Add domain redirect: vdh-agency.nl → www.vdh-agency.com

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,23 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{type: 'host', value: 'vdh-agency.nl'}],
+        destination: 'https://www.vdh-agency.com/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [{type: 'host', value: 'www.vdh-agency.nl'}],
+        destination: 'https://www.vdh-agency.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default withNextIntl(nextConfig);
